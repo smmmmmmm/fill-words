263a1601a49e692949f6ed3c22bbd793d5b6a0fd
smmmmmmm <simasima442317@gmail.com>
2022-03-29 23:45:50 +0900

    Fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Top from './components/Top';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='' element={<Top/>} />
         <Route path='/play' element={<Play/>} />
